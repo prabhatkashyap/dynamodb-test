30b3f13d3a21e73c0472cfdd73a11aa72b7b1a7b
prabhat <prabhat@nexthoughts.com>
2018-01-26 20:21:47 +0530

    ..

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -4,10 +4,10 @@ includeTargets << grailsScript("_GrailsCompile")
 eventCompileStart = {
     ant.delete(file: "${basedir}/web-app/META-INF/*.xml")
 
-    if (grails.util.Environment.current.name == "production") {
-        println("*********=========== Session Script ============************")
-        ant.copy(tofile: "${basedir}/web-app/META-INF/context.xml") {
-            fileset(file: "${basedir}/context-prod.xml")
-        }
-    }
+//    if (grails.util.Environment.current.name == "production") {
+//        println("*********=========== Session Script ============************")
+//        ant.copy(tofile: "${basedir}/web-app/META-INF/context.xml") {
+//            fileset(file: "${basedir}/context-prod.xml")
+//        }
+//    }
 }
